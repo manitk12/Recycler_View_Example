6f0dd8cb090e38a6bc42da4b75360b598b996472
manit <ermanit007@gmail.com>
2017-07-27 17:37:01 +0530

    Added pojo class in Vcs.xml

---
+++ app/src/main/java/com/grademojo/recycler_view_example/Simple_pojo.java
@@ -0,0 +1,19 @@
+package com.grademojo.recycler_view_example;
+
+/**
+ * Created by sapling_a0 on 27/7/17.
+ */
+
+public class Simple_pojo {
+
+    private String values;
+
+
+    public String getValues() {
+        return values;
+    }
+
+    public void setValues(String values) {
+        this.values = values;
+    }
+}
